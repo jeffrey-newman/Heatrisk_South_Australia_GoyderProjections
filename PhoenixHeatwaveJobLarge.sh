773b1b868fa5f8bee29ac530f9edd47f8b36613b
Jeffrey Newman <jeffrey.newman.au@gmail.com>
2017-04-06 13:32:56 +0930

    Slurm job files for Phoenix
    Corrections to flags so that they actually run
    
    Two job file s- a small tester and a production run.

---
+++ PhoenixHeatwaveJobLarge.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=heatwave_projections_py_mpi      # Job name
+#SBATCH -p batch        # partition (this is the queue your job will be added to) 
+#SBATCH --ntasks=64                  # Number of MPI ranks
+#SBATCH --nodes=16                    # Number of nodes
+#SBATCH --mem-per-cpu=3000mb          # Memory per processor
+#SBATCH --time=36:00:00 # time allocation, which has the format (D-HH:MM), here set to 36 hours
+#SBATCH --mail-type=ALL              # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=jeffrey.newman@adelaide.edu.au  # Where to send mail
+
+pwd; hostname; date
+
+echo "Running prime number generator program on $SLURM_JOB_NUM_NODES nodes with $SLURM_NTASKS tasks, each with $SLURM_CPUS_PER_TASK cores."
+
+module load Python/3.6.0-foss-2016uofa
+module load R/3.3.0-foss-2016uofa
+module load GDAL/2.1.0-foss-2016uofa
+
+mpirun -np 64 python "/home/a1091793/Code/HeatwaveAnalaysis/Heatrisk_South_Australia_GoyderProjections/ParallelHeatRiskAnalysis2Phoenix.py"
